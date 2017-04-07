da0971229dc35610cc08a08a8a37f87a949ad558
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-07 18:20:00 +0700

    init

---
+++ index.ios.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from './src/components/App';
+
+export default class DemoB2 extends Component {
+  render() {
+    return <App />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('DemoB2', () => DemoB2);
